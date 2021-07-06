cf26c064e681d68154c926b8ad04d95a105a5860
lining1111 <770677655@qq.com>
2021-07-06 18:03:39 +0800

    nfc step1

--- include/nfc/NFC_Operate.h
+++ include/nfc/NFC_Operate.h
@@ -6,17 +6,53 @@
 #define _NFC_OPERATE_H
 
 #include <nfc/nfc.h>
+#include "nfc/nfc-utils.h"
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class NFC_Operate {
 public:
+#define MAX_DEVICE_COUNT 16
+#define MAX_TARGET_COUNT 16
     nfc_context *context = nullptr;
-    nfc_connstring devices[8];
+    nfc_connstring devices[MAX_DEVICE_COUNT];
     size_t device_count = 0;
     int device_index = 0;
     nfc_device *nfcDevice = nullptr;
+    /**
+     * 支持卡面的种类,会影响nfc_modulation的设置
+     * 0x01:NMT_ISO14443A
+     * 0x02:NMT_FELICA(212)
+     * 0x04:NMT_FELICA(424)
+     * 0x08:NMT_ISO14443B
+     * 0x10:NMT_ISO14443BI
+     * 0x20:NMT_ISO14443B2SR
+     * 0x40:NMT_ISO14443B2CT
+     * 0x80:NMT_ISO14443BICLASS
+     * 0x100:NMT_JEWEL
+     * 0x200:NMT_BARCODE
+     */
+    int mask = 0x3ff;
+    nfc_modulation nm;
+
+    bool verbose = false;//是否开启信息打印
+
+
+    //卡面信息集合
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_FELICA_212;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+    vector<nfc_target> vector_ISO14443A_106;
+
+
 
     string tag_uid;//curr cardid
 
@@ -35,16 +71,36 @@ public:
     /**
      * 选择一个读卡器
      * @param index in 读卡器索引
+     * @return 0:success -1:fail
      */
-    void SelectDevice(int index);
+    int SelectDevice(int index);
+
+    /**
+     * 列出所有连接的卡
+     * @param mask in 支持的卡种类的掩码
+     * @return 连接的卡的数量
+     */
+    int ListNfcCard(int mask);
 
     /**
      * 获取卡id
+     * @param card_index in 卡索引
      * @param uid out 卡id数据
      * @param uid_len 卡id数组最大长度
      * @return 卡id数据长度
+     * @brief 在执行过ListNfcCard函数后，获取了卡列表(nfc_target ant)后可以根据索引值获取，否则需要执行ListNfcCard
      */
-    int GetSelectCardUid(uint8_t *uid, int uid_len);
+    int GetSelectCardUid(int card_index, uint8_t *uid, int uid_len);
+
+    /**
+    * 获取卡id
+    * @param nm in 卡类型属性
+    * @param uid out 卡id数据
+    * @param uid_len 卡id数组最大长度
+    * @return 卡id数据长度
+    */
+    int GetSelectCardUid(nfc_modulation nm, uint8_t *uid, int uid_len);
+
 
     /**
      * 是否是扇区内的第一个块区 (第1扇区的第1块，是存放uid的地方，写入时要慎重)
--- src/nfc/NFC_Operate.cpp
+++ src/nfc/NFC_Operate.cpp
@@ -5,7 +5,7 @@
 #include "nfc/NFC_Operate.h"
 
 NFC_Operate::NFC_Operate() {
-
+    bzero(devices, sizeof(devices));
 }
 
 NFC_Operate::~NFC_Operate() {
@@ -13,14 +13,233 @@ NFC_Operate::~NFC_Operate() {
 }
 
 int NFC_Operate::Open() {
+    //1.初始化nfc
+    nfc_init(&context);
+    if (context == nullptr) {
+        printf("Unable to init libnfc (malloc).\n");
+        return -1;
+    }
+
+    const char *acLibnfcVersion = nfc_version();
+    printf("use libnfc version:%s\n", acLibnfcVersion);
+
+
+    //2.获取读卡器列表
+    device_count = nfc_list_devices(context, devices, sizeof(devices));
+    if (device_count == 0) {
+        printf("No NFC device found.\n");
+        return -1;
+    }
+    //至此完成读卡器列表读取，等待选择读卡器索引 device_index，默认为0
+
     return 0;
 }
 
-void NFC_Operate::SelectDevice(int index) {
+int NFC_Operate::SelectDevice(int index) {
+    if (index > device_count) {
+        printf("index:%s is too big,max:%d \n", index, device_count - 1);
+        printf("set default index:%d\n", device_index);
+    } else {
+        device_index = index;
+    }
+    nfcDevice = nfc_open(context, devices[index]);
 
+    if (nfcDevice == nullptr) {
+        printf("Unable to open NFC device: %s\n", devices[index]);
+        return -1;
+    }
+    if (nfc_initiator_init(nfcDevice) < 0) {
+        printf("nfc_initiator_init fail\n");
+        return -1;
+    }
+    printf("NFC device: %s opened\n", nfc_device_get_name(nfcDevice));
+
+    return 0;
 }
 
-int NFC_Operate::GetSelectCardUid(uint8_t *uid, int uid_len) {
+int NFC_Operate::ListNfcCard(int mask) {
+    int res = 0;
+    nfc_target ant[MAX_TARGET_COUNT];
+    unsigned int ant_num = 0;
+    int index = 0;
+    if (mask & 0x1) {
+        nm.nmt = NMT_ISO14443A;
+        nm.nbr = NBR_106;
+        // List ISO14443A targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443A passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x02) {
+        nm.nmt = NMT_FELICA;
+        nm.nbr = NBR_212;
+        // List Felica tags
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d Felica (212 kbps) passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x04) {
+        nm.nmt = NMT_FELICA;
+        nm.nbr = NBR_424;
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d Felica (424 kbps) passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x08) {
+        nm.nmt = NMT_ISO14443B;
+        nm.nbr = NBR_106;
+        // List ISO14443B targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443B passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x10) {
+        nm.nmt = NMT_ISO14443BI;
+        nm.nbr = NBR_106;
+        // List ISO14443B' targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443B' passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x20) {
+        nm.nmt = NMT_ISO14443B2SR;
+        nm.nbr = NBR_106;
+        // List ISO14443B-2 ST SRx family targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443B-2 ST SRx passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x40) {
+        nm.nmt = NMT_ISO14443B2CT;
+        nm.nbr = NBR_106;
+        // List ISO14443B-2 ASK CTx family targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443B-2 ASK CTx passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x80) {
+        nm.nmt = NMT_ISO14443BICLASS;
+        nm.nbr = NBR_106;
+        // List ISO14443B iClass targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443B iClass passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x100) {
+        nm.nmt = NMT_JEWEL;
+        nm.nbr = NBR_106;
+        // List Jewel targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443A-3 Jewel passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    if (mask & 0x200) {
+        nm.nmt = NMT_BARCODE;
+        nm.nbr = NBR_106;
+        // List NFC Barcode targets
+        if ((res = nfc_initiator_list_passive_targets(nfcDevice, nm, &ant[index], MAX_TARGET_COUNT - index)) >= 0) {
+            if (verbose || (res > 0)) {
+                printf("%d ISO14443A-2 NFC Barcode passive target(s) found%s\n", res, (res == 0) ? ".\n" : ":");
+            }
+            for (int i = 0; i < res; i++) {
+                print_nfc_target(&ant[index + i], verbose);
+                printf("\n");
+            }
+        }
+    }
+    index += res;
+
+    ant_num = index;
+    return index;
+}
+
+int NFC_Operate::GetSelectCardUid(int card_index, uint8_t *uid, int uid_len) {
+    if (ant_num == 0) {
+        //未获取过卡列表
+        ListNfcCard(this->mask);
+    }
+    if (card_index > (ant_num - 1)) {
+        printf("card index %d is big than max:%d\n", card_index, ant_num - 1);
+        return -1;
+    }
+    memcpy(uid, ant[card_index].nti.)
+
+    return 0;
+}
+
+int NFC_Operate::GetSelectCardUid(nfc_modulation nm, uint8_t *uid, int uid_len) {
     return 0;
 }
 
@@ -54,12 +273,14 @@ int NFC_Operate::WriteData(uint32_t blockNum, uint8_t *data, int data_size) {
 
 int NFC_Operate::Close() {
 
-    if (nfcDevice){
+    if (nfcDevice) {
         nfc_close(nfcDevice);
+        nfcDevice = nullptr;
     }
 
-    if (context){
+    if (context) {
         nfc_exit(context);
+        context = nullptr;
     }
 
     return 0;
